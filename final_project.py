df00333c157eda453f2fa6b3474921a8e89c630d
nerijus-c <149934123+nerijus-c@users.noreply.github.com>
2023-11-14 10:42:13 +0200

    antra diena grafikai ir pan

--- final_project.py
+++ final_project.py
@@ -9,7 +9,7 @@ df['PM10'].interpolate(method='linear', inplace=True)
 df['NO2'].interpolate(method='linear', inplace=True)
 
 df = df.drop(columns=['ISO3', 'Reference', 'Database_version'])
-df = df.replace("Šiauliai", "Siauliai")
+df = df.replace('Šiauliai', 'Siauliai')
 df = df.replace('Klaipėda', 'Klaipeda')
 df = df.replace('Kėdainiai', 'Kedainiai')
 df = df.replace('Mažeikiai', 'Mazeikiai')
@@ -21,12 +21,12 @@ df['PM10'] = df['PM10'].astype(int)
 df['NO2'] = df['NO2'].astype(int)
 df['Country'] = df['Country'].astype(str)
 df['City'] = df['City'].astype(str)
-# df['Year'] = pd.to_datetime(df['Year']).dt.year
+# df['Year'] = pd.to_datetime(df['Year']).dt.strftime('%Y%')
 
 # df.to_csv('C:/Users/Vartotojas/Documents/GitHub/Final-project/csv/who_data_100_interp.csv', index=False)
 # df=df.dtypes
+# print(df.dtypes)
 # print(df)
-
 # avg_all_PM25 = df[['PM2.5']].mean().round(2)
 # avg_all_PM10 = df[['PM10']].mean().round(2)
 # avg_all_NO2 = df[['NO2']].mean().round(2)
@@ -60,7 +60,7 @@ avg_city_NO2 = df.groupby('City', as_index=False)[['NO2']].mean().round(2)
 def show_air_quality_statistics_by_year():
     # Adding x variable for X-Axis "Date" values
     x = avg_year['Year']
-    plt.figure(figsize=(14, 10))
+    plt.figure(figsize=(14, 12))
     plt.plot(x, avg_year['PM2.5'], label='PM2.5', color='green')
     plt.plot(x, avg_year['PM10'], label='PM10', color='blue')
     plt.plot(x, avg_year['NO2'], label='NO2', color='red')
@@ -77,6 +77,7 @@ def show_air_quality_statistics_by_year():
     plt.title('Air quality by year', fontsize=20)
     plt.xticks(x, rotation=90)
     plt.grid()
+    plt.savefig('C:/Users/Vartotojas/Documents/GitHub/Final-project/jpeg/air_stat_by_year')
     plt.show()
 
 # show_air_quality_statistics_by_year()
@@ -93,30 +94,75 @@ def show_city_PM25_average():
     plt.title('Yearly average PM2.5 by city', fontsize=18)
     plt.xticks(x, rotation=60)
     plt.rcParams.update({'font.size': 22})
+    plt.savefig('C:/Users/Vartotojas/Documents/GitHub/Final-project/jpeg/city_pm25_avg')
     plt.show()
 
-show_city_PM25_average()
+# show_city_PM25_average()
 
 def show_city_PM10_average():
     x = avg_city_PM10['City']
-    plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(12, 18))
     plt.bar(x, avg_city_PM10['PM10'], color='blue')
     plt.ylabel('Value μg/m3', fontsize=18)
     plt.title('Yearly average PM10 by city', fontsize=18)
     plt.xticks(x, rotation=60)
     plt.rcParams.update({'font.size': 22})
+    plt.savefig('C:/Users/Vartotojas/Documents/GitHub/Final-project/jpeg/city_PM10_avg')
     plt.show()
 
 # show_city_PM10_average()
 
 def show_city_NO2_average():
     x = avg_city_NO2['City']
-    plt.figure(figsize=(12, 12))
+    plt.figure(figsize=(12, 16))
     plt.bar(x, avg_city_NO2['NO2'], color='red')
     plt.ylabel('Value μg/m3', fontsize=18)
     plt.title('Yearly average NO2 by city', fontsize=18)
     plt.xticks(x, rotation=60)
     plt.rcParams.update({'font.size': 22})
+    plt.savefig('C:/Users/Vartotojas/Documents/GitHub/Final-project/jpeg/city_NO2_avg')
     plt.show()
 
 # show_city_NO2_average()
+
+live_df = pd.read_csv('C:/Users/Vartotojas/Documents/GitHub/Final-project/csv/live_data.csv')
+# print(live_df)
+filter_vilnius = live_df.loc[live_df['Miestas']=='Vilnius']
+filter_kaunas = live_df.loc[live_df['Miestas']=='Kaunas']
+filter_klaipeda = live_df.loc[live_df['Miestas']=='Klaipėda']
+# print(filter_klaipeda)
+
+def show_air_quality__by_city():
+    # Adding x variable for X-Axis "Date" values
+    plt.figure(figsize=(14, 12))
+    x = filter_kaunas['Date']
+    plt.plot(x, filter_kaunas['PM2.5'], label='PM2.5 Kaunas', color='green')
+    plt.plot(x, filter_kaunas['PM10'], label='PM10 Kaunas', color='blue')
+    plt.plot(x, filter_kaunas['O3'], label='O3 Kaunas', color='red')
+
+    x = filter_vilnius['Date']
+    plt.plot(x, filter_vilnius['PM2.5'], label='PM2.5 Vilnius',linestyle=(0, (5,10)), color='green')
+    plt.plot(x, filter_vilnius['PM10'], label='PM10 Vilnius',linestyle=(0, (5,10)), color='blue')
+    plt.plot(x, filter_vilnius['O3'], label='O3 Vilnius',linestyle=(0, (5,10)), color='red')
+
+    x = filter_klaipeda['Date']
+    plt.plot(x, filter_klaipeda['PM2.5'], label='PM2.5 Klaipėda', linestyle=(0, (1, 1)), color='green')
+    plt.plot(x, filter_klaipeda['PM10'], label='PM10 Klaipėda', linestyle=(0, (1, 1)), color='blue')
+    plt.plot(x, filter_klaipeda['O3'], label='O3 Klaipėda', linestyle=(0, (1, 1)), color='red')
+
+
+    # add legend
+    plt.legend(title='Air quality measures')
+
+    # adding x and y axes with labels and a title
+    plt.ylabel('Value μg/m3', fontsize=18)
+    plt.xlabel('Date', fontsize=18)
+    plt.title('Air quality measures in major cities', fontsize=20)
+    plt.xticks(x, rotation=90)
+    plt.grid()
+    plt.savefig('C:/Users/Vartotojas/Documents/GitHub/Final-project/jpeg/air_quality_by_city')
+    plt.show()
+
+show_air_quality__by_city()
+
+# filtered_avg_lithuania_city = avg_country_city.loc['Lithuania']
